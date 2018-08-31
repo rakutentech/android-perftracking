b563089cdd65b73b2d8315ce5b5b0baf6a548d2f
Akhmad Fathonih <akhmad.fathonih@rakuten.com>
2018-08-31 13:53:58 +0900

    Maintain current behavior to toggle tracker

--- performance-tracking/src/main/java/com/rakuten/tech/mobile/perf/runtime/internal/RuntimeContentProvider.java
+++ performance-tracking/src/main/java/com/rakuten/tech/mobile/perf/runtime/internal/RuntimeContentProvider.java
@@ -87,8 +87,11 @@ public class RuntimeContentProvider extends ContentProvider {
 
     boolean appDebuggable = Util.isAppDebuggable(context);
     double enablePercent = lastConfig.getEnablePercent();
-    double randomNumber = new Random(System.currentTimeMillis()).nextDouble() * 100.0;
-    boolean enableTracking = (randomNumber < enablePercent);
+    boolean enableTracking = false;
+    if (enablePercent > 0.0) {
+      double randomNumber = new Random(System.currentTimeMillis()).nextDouble() * 100.0;
+      enableTracking = (randomNumber <= enablePercent);
+    };
     if (appDebuggable || enableTracking) {
       config = new Config();
       config.app = packageName;
